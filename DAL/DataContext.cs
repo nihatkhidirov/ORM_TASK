28162a15e2f0f7326777dfd0ecc150dbbd84e5b9
nihatkhidirov <nihatmkh@code.edu.az>
2024-04-22 15:55:42 +0400

    First Commit

---
+++ DAL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ORM_TASK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_TASK.DAL
+{
+
+    internal class DataContext : DbContext
+    {
+
+        public DbSet<Group> Groups { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP-77U60MC\\SQLEXPRESS; Database=Academy;Trusted_Connection=True");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using ORM_TASK.DAL;
+using ORM_TASK.Models;
+
+using DataContext dataContext = new ();
+var groups= dataContext.Groups.ToList();
+foreach (Group group in groups)
+{
+    Console.WriteLine(group);
+}
